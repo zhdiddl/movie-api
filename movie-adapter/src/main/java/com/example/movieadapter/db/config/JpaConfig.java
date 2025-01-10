ae19a521d320e01b69dfe933281eb7b9987780b6
jina <jinahoi@naver.com>
2025-01-10 22:50:48 +0900

    feat(adapter): 사용자 정보 자동 추가를 위해 auditing 기능 추가
    - `@EnableJpaAuditing`를 활성화하여 JPA Auditing 기능을 사용하도록 설정
    - 현재는 "System"으로 고정되어 있지만 실제 사용자 정보를 가져오는 로직 추가 가능

---
+++ movie-adapter/src/main/java/com/example/movieadapter/db/config/JpaConfig.java
@@ -0,0 +1,19 @@
+package com.example.movieadapter.db.config;
+
+import java.util.Optional;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        // 현재 사용자는 임의로 고정, 실제 사용자 정보를 불러오는 로직 작성 가능
+        return () -> Optional.of("System");
+    }
+
+}

b1d6058d9c506d829f0008b631075f46d87cd880
jina <jinahoi@naver.com>
2025-01-13 00:52:59 +0900

    chore: 불필요한 주석 및 애노테이션 제거
    - JpaRepository를 상속하는 인터페이스에 불필요한 애노테이션 제거
    - 작성한 코드 기능에 적절하지 않다고 판단되는 주석 삭제
    - 빈 스캔 관련 중복으로 설정된 코드 삭제

--- movie-adapter/src/main/java/com/example/movieadapter/db/MovieJpaRepository.java
+++ movie-adapter/src/main/java/com/example/movieadapter/db/MovieJpaRepository.java
@@ -2,8 +2,6 @@ package com.example.movieadapter.db;
 
 import com.example.moviedomain.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MovieJpaRepository extends JpaRepository<Movie, Long> {
 }
--- movie-api/src/main/java/com/example/movieapi/MovieApiApplication.java
+++ movie-api/src/main/java/com/example/movieapi/MovieApiApplication.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "com.example.moviedomain.entity") // 엔티티 스캔
 @EnableJpaRepositories(basePackages = "com.example.movieadapter.db") // 레포지토리 스캔
 @ComponentScan(basePackages = {"com.example"}) // 모든 모듈의 빈 스캔
-@SpringBootApplication(scanBasePackages = "com.example")
+@SpringBootApplication
 public class MovieApiApplication {
 
 	public static void main(String[] args) {

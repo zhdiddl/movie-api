ec6d9d1d5879884da90dd79cafc084b3863f64e5
jina <jinahoi@naver.com>
2025-01-18 20:28:07 +0900

    chore: 스키마 타입 수정으로 DB 효율 개선
    - 필드 특성에 맞는 스토리지 사용으로 스토리지를 효율적으로 사용
    - 장르 필드에 TinyInt Unsigned 사용으로 255 이하 범위 정수 저장
    - 상영 길이 필드에 SmallInt Unsigned 적용해 65,535 이하 범위 정수 저장
    - 그 외 필드도 Varchar 길이를 필드 특성에 맞게 조절
    - 환경 설정 파일에서 스키마 sql 파일을 사용해 DDL 실행 설정

--- movie-infrastructure/src/main/resources/v1.0__initial_schema.sql
+++ movie-infrastructure/src/main/resources/v1.0__initial_schema.sql
@@ -1,48 +1,48 @@
 CREATE TABLE movie (
-                       id              INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,  -- BIGINT 대신 UNSIGNED INT 로 리소스 절약
-                       title           VARCHAR(255) NOT NULL,
-                       genre           VARCHAR(255) NOT NULL,
-                       content_rating  VARCHAR(255) NOT NULL,
-                       release_date    DATE         NOT NULL,
-                       runtime_minutes INT          NOT NULL,
+                       id              INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                       title           VARCHAR(150) NOT NULL,
+                       genre           TINYINT UNSIGNED NOT NULL,
+                       content_rating  CHAR(1) NOT NULL,
+                       release_date    DATE NOT NULL,
+                       runtime_minutes SMALLINT UNSIGNED NOT NULL,
                        thumbnail_url   VARCHAR(255) NOT NULL,
-                       created_at      DATETIME(6)  NOT NULL,
-                       created_by      VARCHAR(255) NOT NULL,
-                       modified_at     DATETIME(6)  NOT NULL,
-                       modified_by     VARCHAR(255) NOT NULL
+                       created_at      DATETIME(6) NOT NULL,
+                       created_by      VARCHAR(50) NOT NULL,
+                       modified_at     DATETIME(6) NOT NULL,
+                       modified_by     VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE theater (
-                         id          INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,  
-                         name        VARCHAR(255) NOT NULL,
-                         created_at  DATETIME(6)  NOT NULL,
-                         created_by  VARCHAR(255) NOT NULL,
-                         modified_at DATETIME(6)  NOT NULL,
-                         modified_by VARCHAR(255) NOT NULL
+                         id          INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                         name        VARCHAR(50) NOT NULL,
+                         created_at  DATETIME(6) NOT NULL,
+                         created_by  VARCHAR(50) NOT NULL,
+                         modified_at DATETIME(6) NOT NULL,
+                         modified_by VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE screening (
-                           id          INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,  
-                           movie_id    INT UNSIGNED NOT NULL,                   
-                           theater_id  INT UNSIGNED NOT NULL,                   
+                           id          INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                           movie_id    INT UNSIGNED NOT NULL,
+                           theater_id  INT UNSIGNED NOT NULL,
                            start_time  TIME(6) NOT NULL,
                            end_time    TIME(6) NOT NULL,
-                           created_at  DATETIME(6)  NOT NULL,
-                           created_by  VARCHAR(255) NOT NULL,
-                           modified_at DATETIME(6)  NOT NULL,
-                           modified_by VARCHAR(255) NOT NULL,
+                           created_at  DATETIME(6) NOT NULL,
+                           created_by  VARCHAR(50) NOT NULL,
+                           modified_at DATETIME(6) NOT NULL,
+                           modified_by VARCHAR(50) NOT NULL
 --                            CONSTRAINT FK_screening_theater FOREIGN KEY (theater_id) REFERENCES theater (id),
 --                            CONSTRAINT FK_screening_movie FOREIGN KEY (movie_id) REFERENCES movie (id)
 );
 
 CREATE TABLE seat (
-                      id          INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,  
-                      theater_id  INT UNSIGNED NOT NULL,                   
-                      seat_row    VARCHAR(255) NULL,
-                      seat_column INT NULL,
-                      created_at  DATETIME(6)  NOT NULL,
-                      created_by  VARCHAR(255) NOT NULL,
-                      modified_at DATETIME(6)  NOT NULL,
-                      modified_by VARCHAR(255) NOT NULL,
+                      id          INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                      theater_id  INT UNSIGNED NOT NULL,
+                      seat_row    VARCHAR(1) NOT NULL,  -- A ~ Z 로 제한 가능
+                      seat_column TINYINT UNSIGNED NOT NULL,  -- 1 ~ 99 범위 사용
+                      created_at  DATETIME(6) NOT NULL,
+                      created_by  VARCHAR(50) NOT NULL,
+                      modified_at DATETIME(6) NOT NULL,
+                      modified_by VARCHAR(50) NOT NULL
 --                       CONSTRAINT FK_seat_theater FOREIGN KEY (theater_id) REFERENCES theater (id)
 );

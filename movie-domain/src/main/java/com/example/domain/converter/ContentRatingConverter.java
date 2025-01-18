0fbe0bf5f9f29e180858117678afe728c5bf5532
jina <jinahoi@naver.com>
2025-01-18 20:10:35 +0900

    refactor: DB 값으로 Enum 조회 방식 최적화
    - DB에 저장된 값을 Enum으로  변환하는 메서드 조회 방식 수정
    - Stream 필터링에서 Map 기반으로 검색 최적화
    - Optional 제거로 불필요한 객체 생성 방지

--- movie-domain/src/main/java/com/example/domain/converter/ContentRatingConverter.java
+++ movie-domain/src/main/java/com/example/domain/converter/ContentRatingConverter.java
@@ -7,8 +7,6 @@ import jakarta.persistence.Converter;
 @Converter(autoApply = true) // 모든 ContentRating 필드에 자동으로 적용
 public class ContentRatingConverter implements AttributeConverter<ContentRating, String> {
 
-    private static final ContentRating DEFAULT_RATING = ContentRating.UNKNOWN;
-
     @Override
     public String convertToDatabaseColumn(ContentRating contentRating) {
         assert contentRating != null : "[ERROR] ContentRating is null.";
@@ -18,8 +16,7 @@ public class ContentRatingConverter implements AttributeConverter<ContentRating,
     @Override
     public ContentRating convertToEntityAttribute(String dbData) {
         assert dbData != null : "[ERROR] ContentRating from DB value is null.";
-        return ContentRating.fromDbValue(dbData)
-                .orElse(DEFAULT_RATING);
+        return ContentRating.fromDbValue(dbData);
     }
 
 }
--- movie-domain/src/main/java/com/example/domain/model/valueObject/ContentRating.java
+++ movie-domain/src/main/java/com/example/domain/model/valueObject/ContentRating.java
@@ -1,7 +1,10 @@
 package com.example.domain.model.valueObject;
 
-import java.util.Arrays;
-import java.util.Optional;
+import com.example.domain.exception.CustomException;
+import com.example.domain.exception.ErrorCode;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.Getter;
 
 @Getter
@@ -11,7 +14,6 @@ public enum ContentRating {
     TWELVE("12세 이상 관람가", 12, "B"),
     FIFTEEN("15세 이상 관람가", 15, "C"),
     ADULT("청소년 관람 불가", 18, "D"),
-    UNKNOWN("알 수 없는 등급", 0, "Z"),
     ;
 
     private final String description;
@@ -24,13 +26,15 @@ public enum ContentRating {
         this.dbValue = dbValue;
     }
 
-    public static Optional<ContentRating> fromDbValue(String dbValue) {
+    private static final Map<String, ContentRating> MAP =
+            Stream.of(values()).collect(Collectors.toMap(ContentRating::getDbValue, contentRating -> contentRating));
+
+    public static ContentRating fromDbValue(String dbValue) {
+        ContentRating contentRating = MAP.get(dbValue);
         if (dbValue == null) {
-            return Optional.empty();
+            throw new CustomException(ErrorCode.INVALID_CONTENT_RATING);
         }
-        return Arrays.stream(values())
-                .filter(contentRating -> contentRating.getDbValue().equals(dbValue))
-                .findFirst();
+        return contentRating;
     }
 
 }

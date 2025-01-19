71ec9c06ab56ea6e2044a729e8ee8e1a6ce9a96a
jina <jinahoi@naver.com>
2025-01-20 02:39:53 +0900

    feat: Caffeine 캐시 적용 및 설정 추가
    - Caffeine 캐시 라이브러리 의존성 추가
    - `CacheConfig`설정 클래스 추가해 캐시 전략 설정
    - 검색 조회 서비스 메서드에 Cacheable 애노테이션 적용
    - Join 연산을 줄이는 목적으로 캐싱을 활용
    - 결과 데이터 크기가 작고 변경 빈도가 낮으므로 결과 전체를 캐싱

---
+++ movie-infrastructure/src/main/java/com/example/infrastructure/db/config/CacheConfig.java
@@ -0,0 +1,30 @@
+package com.example.infrastructure.db.config;
+
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.concurrent.TimeUnit;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(20, TimeUnit.MINUTES) // 20분 후 만료
+                .maximumSize(10000); // 최대 캐시 저장 개수
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(caffeine);
+        return cacheManager;
+    }
+
+}

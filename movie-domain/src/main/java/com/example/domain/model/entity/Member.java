c441a56d0ea62eddcea23c5cba26d988f657c088
jina <jinahoi@naver.com>
2025-01-25 08:44:41 +0900

    feat: 영화 예약 API에 필요한 엔티티 추가
    - `Member`: 회원 정보 관리
    - `Reservation`: 회원이 생성한 예약 정보를 관리
    - `SeatReservation`: 특정 예약에 대한 좌석 정보를 관리
    - 좌석과 예약의 다대다 관계에서 `SeatReservation`이 중간테이블 역할을 수행

---
+++ movie-domain/src/main/java/com/example/domain/model/entity/Member.java
@@ -0,0 +1,34 @@
+package com.example.domain.model.entity;
+
+import com.example.domain.model.base.AuditingFields;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    protected Member() {}
+
+    private Member(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public static Member of(String name, String email) {
+        return new Member(name, email);
+    }
+
+}
---
+++ movie-domain/src/main/java/com/example/domain/model/entity/Reservation.java
@@ -0,0 +1,47 @@
+package com.example.domain.model.entity;
+
+import com.example.domain.model.base.AuditingFields;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Reservation extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Screening screening;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Member member;
+
+    @OneToMany(mappedBy = "reservation", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SeatReservation> seatReservations = new ArrayList<>();
+
+    protected Reservation() {}
+
+    private Reservation(Screening screening, Member member) {
+        this.screening = screening;
+        this.member = member;
+    }
+
+    public static Reservation create(Screening screening, Member member) {
+        return new Reservation(screening, member);
+    }
+
+}

45aa0a5253dfd17bc73a8c03ca7e89fb972856e9
jina <jinahoi@naver.com>
2025-02-04 01:38:05 +0900

    refactor: 사용하지 않는 메서드 및 클래스 제거
    - 리팩토링 과정에서 사용하지 않는 코드 제거

--- movie-infrastructure/src/main/java/com/example/infrastructure/db/MovieJpaRepository.java
+++ movie-infrastructure/src/main/java/com/example/infrastructure/db/MovieJpaRepository.java
@@ -1,26 +1,7 @@
 package com.example.infrastructure.db;
 
 import com.example.domain.model.entity.Movie;
-import com.example.domain.model.projection.MovieProjection;
-import com.example.domain.model.valueObject.Genre;
-import java.util.List;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface MovieJpaRepository extends JpaRepository<Movie, Long> {
-
-    @Query("""
-    SELECT m 
-    FROM Movie m 
-    WHERE (:title IS NULL OR LOWER(m.title) LIKE LOWER(CONCAT('%', :title, '%'))) 
-      AND (:genre IS NULL OR m.genre = :genre)
-""")
-    List<MovieProjection> findByTitleContainingIgnoreCaseAndGenre(
-            @Param("title") String title,
-            @Param("genre") Genre genre,
-            Sort sort
-    );
-
 }
--- movie-infrastructure/src/main/java/com/example/infrastructure/db/SeatJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.infrastructure.db;
-
-import com.example.domain.model.entity.Seat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeatJpaRepository extends JpaRepository<Seat, Long> {
-}

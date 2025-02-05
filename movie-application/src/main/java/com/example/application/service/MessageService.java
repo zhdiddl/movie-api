c128a5598891d13b559e60cdaff5a38526edfa44
jina <jinahoi@naver.com>
2025-02-05 13:51:42 +0900

    refactor: 메시지 전송을 비동기 처리 및 트랜잭션 후 실행하도록 개선
    - Async 애노테이션을 적용하여 메시지 전송을 비동기 처리
    - Event Listenr를 사용하여 예약 메서드 트랜잭션이 커밋된 후 메시지 전송을 보장

--- movie-application/src/main/java/com/example/application/service/MessageService.java
+++ movie-application/src/main/java/com/example/application/service/MessageService.java
@@ -2,12 +2,17 @@ package com.example.application.service;
 
 import com.example.application.port.in.MessageServicePort;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Service
 public class MessageService implements MessageServicePort {
 
+    @Async // 별도 스레드에서 비동기 실행
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT) // 호출한 메서드 트랜잭션이 커밋된 후 실행
     @Override
     public void send(String message) {
         try {
